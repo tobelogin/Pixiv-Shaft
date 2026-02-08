b7525d6fefb6ba4a42e85aa70cf89011926bae2a
zjad <108188072+tobelogin@users.noreply.github.com>
2026-02-08 17:11:18 +0800

    change library name

--- native-libs/src/main/java/my/app/native_libs/FormatConverter.kt
+++ native-libs/src/main/java/my/app/native_libs/FormatConverter.kt
@@ -2,9 +2,8 @@ package my.app.native_libs
 
 class FormatConverter {
     companion object {
-        // Used to load the 'native_libs' library on application startup.
         init {
-            System.loadLibrary("native_libs")
+            System.loadLibrary("av-helper")
         }
 
         fun list2webp(listPath: String, webpPath: String): Int {
